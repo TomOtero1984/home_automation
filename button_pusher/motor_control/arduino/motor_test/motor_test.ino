a969dff74380d12437448d9de52eb55c743ab369
Tom <tomotero1984@gmail.com>
2021-02-17 23:46:51 -0800

    minor changes

--- button_pusher/motor_control/arduino/motor_test/motor_test.ino
+++ button_pusher/motor_control/arduino/motor_test/motor_test.ino
@@ -14,37 +14,67 @@ typedef struct _pwm{
     unsigned long time_curr;
 } _pwm;
 
+//Global
+_pwm pulse;
 
-void setup(_pwm* pulse)
+void setup(void)
 {
+    init_pulse(&pulse);
+}
+
+
+void loop(void)
+{
+    _wait_pulse_duration(&pulse, pulse.refresh_rate);
+
+}
+
+
+void init_pulse(_pwm* pulse)
+{     
     pulse->count = 0;
     pulse->time_start = 0;
     pulse->time_curr = 0;
-    pulse->refresh_rate = 20;
-    pulse->forward_rate = 1.7;
-    pulse->backward_rate = 1.3;
-    pulse->stall_rate = 1.52;
-
-
+    pulse->refresh_rate = 20000; // us
+    pulse->forward_rate = 1700;  // us
+    pulse->backward_rate = 1300; // us
+    pulse->stall_rate = 15200;   // us
 }
 
 
-void loop(_pwm* pulse)
-{
-    wait_refresh_rate(pulse);
-
-}
-
-
-void wait_pulse_duration(_pwm* pulse, int dur)
+void _wait_pulse_duration(_pwm* pulse, int dur)
 {
     int delta = 0;
-    pulse->time_start = millis();
+    pulse->time_start = micros();
     while(delta >= dur)
     {
-        pulse->time_curr = millis();
+        pulse->time_curr = micros();
         delta = pulse->time_curr % pulse->time_start;
     }
+}
+
+
+void forward(_pwm* pulse, int dur)
+{
+    // pin low
+    _wait_pulse_duration(pulse,pulse->refresh_rate);
+    // pin high
+    _wait_pulse_duration(pulse,pulse->forward_rate);
+    // pin low    
+}
+
+
+void backward(_pwm* pulse, int dur)
+{
+    _wait_pulse_duration(pulse,pulse->refresh_rate);
+    _wait_pulse_duration(pulse,pulse->backward_rate);    
+}
+
+
+void stall(_pwm* pulse, int dur)
+{
+    _wait_pulse_duration(pulse,pulse->refresh_rate);
+    _wait_pulse_duration(pulse,pulse->stall_rate);    
 }
 
 
